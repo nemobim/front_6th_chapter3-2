c3e25131e0605943c0ecfd6f0ee9a8b6d9513496
nemo-nemo-bim <drakequation@naver.com>
2025-08-27 22:56:55 +0900

    ✅ test: 주,월,년에 대한 반복 일정 아이콘 컴포넌트 구현

--- src/icons/RepeatIcon.tsx
+++ src/icons/RepeatIcon.tsx
@@ -1,6 +1,6 @@
-// import CalendarToday from '@mui/icons-material/CalendarToday';
-// import CalendarViewMonth from '@mui/icons-material/CalendarViewMonth';
-// import DateRange from '@mui/icons-material/DateRange';
+import CalendarToday from '@mui/icons-material/CalendarToday';
+import CalendarViewMonth from '@mui/icons-material/CalendarViewMonth';
+import DateRange from '@mui/icons-material/DateRange';
 import Repeat from '@mui/icons-material/Repeat';
 
 import { RepeatType } from '../types';
@@ -18,6 +18,14 @@ export function RepeatIcon({ repeatType }: RepeatIconProps) {
     switch (type) {
       case 'daily':
         return <Repeat data-testid="repeat-icon" data-repeat-type="daily" />;
+      case 'weekly':
+        return <DateRange data-testid="repeat-icon" data-repeat-type="weekly" />;
+      case 'monthly':
+        return <CalendarViewMonth data-testid="repeat-icon" data-repeat-type="monthly" />;
+      case 'yearly':
+        return <CalendarToday data-testid="repeat-icon" data-repeat-type="yearly" />;
+      default:
+        return null;
     }
   };
 
