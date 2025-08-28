e0f0cd0c17e330f6d09abe8901c83b731c54c92e
nemo-nemo-bim <drakequation@naver.com>
2025-08-29 03:15:46 +0900

    🐛 fix: CI 오류 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -19,10 +19,6 @@ export default mergeConfig(
       globals: true,
       environment: 'jsdom',
       setupFiles: './src/setupTests.ts',
-      exclude: [
-        '**/node_modules/**',
-        '**/*.e2e.spec.ts', // E2E 테스트 파일 패턴 제외
-      ],
       coverage: {
         reportsDirectory: './.coverage',
         reporter: ['lcov', 'json', 'json-summary'],
